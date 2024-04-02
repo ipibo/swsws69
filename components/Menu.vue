f41bc7e9e611ca51b141e0ce81a845599f67afe4
Ibo <contact@iboibelings.nl>
2024-04-02 13:20:47 +0200

    Add Menu component

---
+++ components/Menu.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="fixed text-2xl">
+    <Button
+      class="bg-primaryRed cursor-pointer text-secondaryRed border-secondaryRed border-[1px]"
+    >
+      <a href="#"> SWSWS69 </a>
+    </Button>
+    <Button
+      class="bg-primaryGreen cursor-pointer text-secondaryGreen border-secondaryGreen border-[1px]"
+    >
+      <a href="#exhibitions"> Exhibitions </a>
+    </Button>
+    <Button
+      class="bg-primaryBlue cursor-pointer text-secondaryBlue border-secondaryBlue border-[1px]"
+    >
+      <a href="#events"> Events </a>
+    </Button>
+  </div>
+</template>
+<script setup></script>
+
+<style></style>
