639de0c71bbfeac30d17bd36a68ab629c4f19edc
Ibo <contact@iboibelings.nl>
2024-04-02 13:20:42 +0200

    Remove console.log and update component structure

--- pages/index.vue
+++ pages/index.vue
@@ -24,7 +24,6 @@ useHead({
 })
 
 onMounted(() => {
-  console.log("mounted")
   const exhibitionsSection = document.getElementById("exhibitions")
   const eventsSection = document.getElementById("events")
   window.addEventListener("scroll", () => {
@@ -47,68 +46,14 @@ watchEffect(() => {
     class="transition ease-in duration-300 p-[32px]"
     :class="backgroundColor"
   >
+    <Menu></Menu>
+
     <div class="font-customFont text-lg mx-auto">
-      <!-- <testPage></testPage> -->
-
       <!-- landingspagina -->
-      <div class="min-h-screen">
-        <h1 class="font-ThreeSix21Pro text-9xl" :class="foregroundColor">
-          SWSWS69
-        </h1>
-
-        <Button class="bg-primaryRed text-secondaryRed 5xl">
-          Now on display</Button
-        >
-        <Button class="bg-primaryGreen text-secondaryGreen"
-          >Now on display</Button
-        >
-        <Button class="bg-primaryBlue text-secondaryBlue"
-          >Now on display</Button
-        >
-
-        <div class="flex flex-col-reverse lg:flex-row">
-          <div
-            class="text-secondaryBlue font-light mx-auto mt-10 text-xl ml-14 md:text-3xl md:mx-24 lg:w-1/2"
-          >
-            <div
-              class="inline bg-secondaryBlue rounded-full text-primaryBlue pl-2 -ml-2 mr-2"
-            >
-              Shared Workspace Weerdjesstraat 69
-            </div>
-            is the co-working space of Erik de Geus, Eva van Boxtel, Guido van
-            der Kooij, Ibo Ibelings, Jelle Reith, Katrijn Westland, Sjef van
-            Beers, Sjoerd Mol & Tess Havas.
-          </div>
-
-          <div>
-            <div
-              class="text-center w-3/4 rounded-xl ml-auto bg-primaryRed mb-10 mx-4"
-            >
-              <img
-                class="rounded-t-xl hidden md:block"
-                src="https://picsum.photos/seed/picsum/2000/1000"
-                alt=""
-              />
-              <div class="font-ThreeSix21Pro text-6xl text-secondaryRed">
-                Exhibitions
-              </div>
-            </div>
-
-            <div class="w-3/4 rounded-xl ml-auto bg-primaryGreen mb-10 mx-4">
-              <img
-                class="rounded-t-xl hidden md:block"
-                src="https://picsum.photos/seed/picsum/2000/1000"
-                alt=""
-              />
-              <div
-                class="text-center font-ThreeSix21Pro text-6xl text-secondaryGreen"
-              >
-                Events
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <LandingsPage
+        id="landingsPage"
+        :currentColor="currentBgColor"
+      ></LandingsPage>
 
       <Exhibitions
         id="exhibitions"
@@ -119,14 +64,13 @@ watchEffect(() => {
       <Events id="events" class="min-h-screen" :class="foregroundColor" />
       <!-- <SliceZone class="grid md:grid-cols-2 grid-cols-1" wrapper="main" :slices="page?.data.slices ?? []"
         :components="components" /> -->
+    </div>
 
-      <div
-        class="fixed bottom-0 left-0 text-8xl ml-4 mb-8 cursor-pointer"
-        @click="accessibility = !accessibility"
-        :class="{ 'text-white': accessibility }"
-      >
-        􁹣
-      </div>
+    <div
+      class="fixed bottom-[32px] left-[32px] w-12 h-12 cursor-pointer z-50"
+      @click="accessibility = !accessibility"
+    >
+      <iconsAccessibility />
     </div>
   </div>
 </template>
