4d38a072ddb8294ad0adbfbe3b29ac188540ee3a
Ibo <contact@iboibelings.nl>
2024-04-02 13:21:16 +0200

    added accessibility icon

---
+++ components/icons/accessibility.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <img class="" :src="importedSvg" alt="Imported SVG" />
+  </div>
+</template>
+
+<script>
+import MySvg from "./assets/svg/accessibility.svg"
+console.log(MySvg)
+
+export default {
+  name: "SvgComponent",
+  data() {
+    return {
+      importedSvg: MySvg,
+    }
+  },
+}
+</script>
+
+<style scoped>
+/* You can add style for your component here */
+</style>
