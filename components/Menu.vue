76a97c41c2ae837fb8dc3c1cf765e14df357948b
Ibo <contact@iboibelings.nl>
2024-04-02 13:42:38 +0200

    Update menu button colors

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="fixed text-2xl top-[1px]">
     <Button
-      class="bg-primaryRed cursor-pointer text-secondaryRed border-secondaryRed border-[1px]"
+      class="bg-primaryBlue cursor-pointer text-secondaryBlue border-secondaryBlue border-[1px]"
     >
       <a href="#"> SWSWS69 </a>
     </Button>
     <Button
-      class="bg-primaryGreen cursor-pointer text-secondaryGreen border-secondaryGreen border-[1px]"
+      class="bg-primaryRed cursor-pointer text-secondaryRed border-secondaryRed border-[1px]"
     >
       <a href="#exhibitions"> Exhibitions </a>
     </Button>
     <Button
-      class="bg-primaryBlue cursor-pointer text-secondaryBlue border-secondaryBlue border-[1px]"
+      class="bg-primaryGreen cursor-pointer text-secondaryGreen border-secondaryGreen border-[1px]"
     >
       <a href="#events"> Events </a>
     </Button>
