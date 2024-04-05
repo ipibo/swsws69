ad3aed8f4f33bd0ff58cab96395d6847915d54f1
Ibo <contact@iboibelings.nl>
2024-04-05 17:50:24 +0200

    Refactor landingsPage.vue to update grid layout, add image, and update text content

--- components/landingsPage.vue
+++ components/landingsPage.vue
@@ -4,16 +4,45 @@
     <Menu currentMenu="SWSWS69" class="px-[32px] md:hidden"></Menu>
     <div class="min-h-screen mt-10 px-[32px]">
       <div class="md:grid md:grid-cols-6 gap-4">
-        <div :class="textColor" class="col-span-4 text-3xl">
+        <div class="col-span-4 text-3xl">
           <img
             class="rounded-xl"
             src="https://images.prismic.io/swsws69/c371a755-f41a-4966-be11-93d35889062f_DSC08301-verkleind.jpg?auto=format,compress"
             alt=""
           />
-          <div>
-            Shared Workspace Weerdjesstraat 69 is the co-working space of Erik
-            de Geus, Eva van Boxtel, Guido van der Kooij, Ibo Ibelings, Jelle
-            Reith, Katrijn Westland, Sjef van Beers, Sjoerd Mol & Tess Havas.
+          <div :class="textColor">
+            Shared Workspace Weerdjesstraat 69 is the co-working space of
+            <a class="blinking-element" href="https://erikdegeus.space/">
+              Erik de Geus</a
+            >,
+            <a class="blinking-element" href="https://evavanboxtel.nl/"
+              >Eva van Boxtel</a
+            >,
+            <a href="https://guidovanderkooij.nl/" class="blinking-element">
+              Guido van der Kooij</a
+            >,
+            <a href="https://iboibelings.nl/" class="blinking-element">
+              Ibo Ibelings</a
+            >,
+            <a href="https://jellereith.nl/" class="blinking-element"
+              >Jelle Reith</a
+            >,
+            <a
+              href="https://www.instagram.com/katrijnwestland"
+              class="blinking-element"
+              >Katrijn Westland</a
+            >,
+            <a href="https://sjefvanbeers.com/" class="blinking-element">
+              Sjef van Beers</a
+            >,
+            <a href="https://sjoerdmol.com/" class="blinking-element"
+              >Sjoerd Mol </a
+            >&
+            <a
+              href="https://www.instagram.com/tesshavas/"
+              class="blinking-element"
+              >Tess Havas</a
+            >.
           </div>
         </div>
 
@@ -61,6 +90,7 @@ const changeColors = () => {
 }
 
 watch(color, (newColor) => {
+  // console.log(color.value)
   changeColors()
 })
 
@@ -113,3 +143,28 @@ changeColors()
           </div>
         </div>
       </div> -->
+
+<style scoped>
+/* Define the blinking animation */
+@keyframes blink {
+  0% {
+    color: inherit;
+  }
+  50% {
+    color: grey;
+  }
+  100% {
+    color: inherit;
+  }
+}
+
+/* Set initial opacity to 1 so it's visible */
+.blinking-element {
+  opacity: 1;
+}
+
+/* Apply the blinking effect on hover */
+.blinking-element:hover {
+  animation: blink 0.5s infinite; /* You can adjust the duration on hover */
+}
+</style>
