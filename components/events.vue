24194053bff151c912e5af71d7108bf31bc4596e
Ibo <contact@iboibelings.nl>
2024-04-11 13:51:15 +0200

    removed lorem ipsum

--- components/events.vue
+++ components/events.vue
@@ -6,10 +6,10 @@
       <!-- <h1 class="text-8xl mt-9 font-ThreeSix21Pro">Events</h1> -->
 
       <text-block class="float-left" :class="textColor">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi a
+        <!-- Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi a
         reiciendis provident, reprehenderit earum odio, sed inventore animi
         fugiat iusto eos unde! Dignissimos, sint mollitia maxime aperiam
-        delectus perspiciatis ipsam.
+        delectus perspiciatis ipsam. -->
       </text-block>
     </div>
   </div>
