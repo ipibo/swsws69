b9ab9be0d82bca5f3fe3cc8bc63920b59935b121
Ibo <contact@iboibelings.nl>
2024-04-17 17:01:58 +0200

    Refactor events.vue to update grid layout and add blocksEvent component

---
+++ components/blocks/event.vue
@@ -0,0 +1,51 @@
+<template>
+  <div
+    class="transition ease-in duration-300 rounded-[40px] border-[2px] text-[36px] leading-[40px]"
+    :class="borderColor"
+  >
+    <div class="p-[40px] transition ease-in duration-300" :class="textColor">
+      <div class="font-bold">{{ exhibitionName }}</div>
+      <div class="font-bold">{{ artistName }}</div>
+      <div class="font-bold">
+        {{ new Date("09-05-2024").toLocaleDateString().replace(/\//g, ".") }}
+
+        <div>14:00 - 17:00</div>
+      </div>
+    </div>
+    <img class="w-full" :src="image" />
+    <div class="p-4" :class="textColor">
+      <div class="mt-4 font-light mb-2 text-[24px]">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { storeToRefs } from "pinia"
+
+const colorStore = useColorStore()
+const { color } = storeToRefs(colorStore)
+const borderColor = ref(`border-secondary${firstCharUpperCase(color.value)}`)
+const textColor = ref(`text-secondary${firstCharUpperCase(color.value)}`)
+
+const changeColors = () => {
+  borderColor.value = `border-secondary${firstCharUpperCase(color.value)}`
+  textColor.value = `text-secondary${firstCharUpperCase(color.value)}`
+}
+
+watch(color, (newColor) => {
+  changeColors()
+})
+
+changeColors()
+
+const props = defineProps({
+  exhibitionName: String,
+  artistName: String,
+  startDate: String,
+  endDate: String,
+  image: String,
+  description: String,
+})
+</script>
--- components/events.vue
+++ components/events.vue
@@ -2,15 +2,38 @@
   <div>
     <titlesEvents class="mb-10" />
     <Menu currentMenu="events" class="mb-10 px-[32px]"></Menu>
-    <div class="min-h-screen px-[32px]">
+    <div class="flex flex-col px-[32px] min-h-screen md:grid md:grid-cols-6">
       <!-- <h1 class="text-8xl mt-9 font-ThreeSix21Pro">Events</h1> -->
 
-      <text-block class="float-left" :class="textColor">
-        <!-- Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi a
-        reiciendis provident, reprehenderit earum odio, sed inventore animi
-        fugiat iusto eos unde! Dignissimos, sint mollitia maxime aperiam
-        delectus perspiciatis ipsam. -->
-      </text-block>
+      <blocksEvent
+        class="transition ease-in duration-300 col-start-1 col-span-4 self-start"
+        exhibitionName="The Hmm ON The Perfect Feed"
+        artistName=""
+        image="https://images.prismic.io/swsws69/Zh_hQ0aI3ufuUQgI_swsws69site.png?auto=format,compress"
+      >
+        Our social media feed has changed quite a lot over the past 15
+        years. What started out as updates of what our friends are doing,
+        became information shared from accounts or pages to us as their
+        followers, and now—especially “For You”—content shows up on our feeds,
+        just because algorithms suspect we’ll find it interesting. We’ve also
+        seen feeds go from permanent to temporary, with concepts like Stories or
+        BeReal popping up. Given that timelines are where many internet users
+        primarily get their information today, taking a closer look
+        at their development is essential.
+
+        <br />
+        For all our fellow users of the Internet, The Hmm developed a workshop
+        in collaboration with Soyun Park and Leo Scarin of RGBdog, that will be
+        available at three different locations. Through The Perfect
+        Feed, we invite you to craft your ultimate social media timeline.
+        <div>
+          <a
+            href="https://thehmm.nl/event/the-hmm-on-the-perfect-feed/"
+            class="px-[16px] rounded-full bg-secondaryGreen text-primaryGreen"
+            >Tickets</a
+          >
+        </div>
+      </blocksEvent>
     </div>
   </div>
 </template>
