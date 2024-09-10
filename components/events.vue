0e410a14dcd3a8f8f621ec75a2f3854839669e06
Ibo <iboibelings@gmail.com>
2024-09-10 11:08:22 +0200

    updated event

--- components/events.vue
+++ components/events.vue
@@ -7,6 +7,34 @@
 
       <blocksEvent
         class="transition ease-in duration-300 col-start-1 col-span-4 self-start"
+        exhibitionName="Open screen at SWSWS69"
+        artistName=""
+        image="https://images.prismic.io/swsws69/ZuAMVBoQrfVKl5eE_openscreenwebsite.png?auto=format,compress"
+      >
+        Come share your favorite online videos on the big screen and enjoy good
+        company and cheap drinks, or bring your own.
+
+        <br />
+        This night we’ll be doing what most us already do on a daily basis:
+        sharing funny, interesting or surprising online videos with each other.
+        But now we’re bringing it in-person to our studio space!
+        <br />
+        Come by on Friday 20th of September, hook up your phone to the big
+        screen at our studio and share what’s in your TikTok likes, Saved posts
+        on Instagram or Twitter Bookmarks. You can shortly introduce your videos
+        if you want, but you don’t have to. So bring your best content and some
+        friends.
+        <!-- 
+        <div>
+          <a
+            href="https://thehmm.nl/event/the-hmm-on-the-perfect-feed/"
+            class="px-[16px] rounded-full bg-secondaryGreen text-primaryGreen"
+            >Tickets</a
+          >
+        </div> -->
+      </blocksEvent>
+      <!-- <blocksEvent
+        class="transition ease-in duration-300 col-start-1 col-span-4 self-start"
         exhibitionName="The Hmm ON The Perfect Feed"
         artistName=""
         image="https://images.prismic.io/swsws69/Zh_hQ0aI3ufuUQgI_swsws69site.png?auto=format,compress"
@@ -33,7 +61,7 @@
             >Tickets</a
           >
         </div>
-      </blocksEvent>
+      </blocksEvent> -->
     </div>
   </div>
 </template>
